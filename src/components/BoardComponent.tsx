3539620eb920da7637c4423e95876456fcae0416
ddubinin <dubinin.dmitry@mail.ru>
2023-03-15 18:30:25 +0300

    feat: added class when cell selected

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { useState } from "react";
 import { Board } from "../models/Board";
+import { Cell } from "../models/Cell";
 import CellComponent from "./CellComponent";
 
 interface BoardProps {
@@ -8,12 +9,27 @@ interface BoardProps {
 }
 
 export default function BoardComponent({ board, setBoard }: BoardProps) {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    if (cell.figure) {
+      setSelectedCell(cell);
+    }
+  }
+
   return (
     <div className="board">
       {board.cells.map((row, index) => (
         <div key={index}>
           {row.map((cell) => (
-              <CellComponent cell={cell} key={cell.id}/>
+            <CellComponent
+              click={click}
+              cell={cell}
+              key={cell.id}
+              selected={
+                cell.x === selectedCell?.x && cell.y === selectedCell?.y
+              }
+            />
           ))}
         </div>
       ))}
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,11 +2,16 @@ import { Cell } from "../models/Cell";
 
 interface CellProps {
   cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-export default function CellComponent({ cell }: CellProps) {
+export default function CellComponent({ cell, selected, click }: CellProps) {
   return (
-    <div className={["cell", cell.color].join(" ")}>
+    <div
+      className={["cell", cell.color, selected ? "selected" : ""].join(" ")}
+      onClick={() => click(cell)}
+    >
       {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
     </div>
   );
